492c50f6d72a1e22f60c246b7745adb9537d98b2
Rafael Cordones <rafael@cordones.me>
2013-12-14 07:50:41 +0100

    #22: Added 505 server error page to debug Heroku issues with Talks section

---
+++ app/Global.scala
@@ -0,0 +1,13 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+object Global extends GlobalSettings {
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    Future.successful(InternalServerError(
+      views.html.serverError(ex)
+    ))
+  }
+}
