a127552e9183814a761c5b1bfcbfdbf4d10375e4
Rafael Cordones <rafael@cordones.me>
2013-12-17 00:37:46 +0100

    #26: added countdown + launch filter

--- app/Global.scala
+++ app/Global.scala
@@ -21,29 +21,29 @@ object Global extends WithFilters(new CountdownFilter) {
  */
 class CountdownFilter extends Filter with Results {
 
-  val launchTime = new DateTime(2013, 12, 17, 18, 30, DateTimeZone.forID("Europe/Vienna"))
+  val launchTime = new DateTime(2013, 12, 17, 19, 1, DateTimeZone.forID("Europe/Vienna"))
 
   def apply(next: (RequestHeader) => Future[SimpleResult])(rh: RequestHeader): Future[SimpleResult] = {
 
-    if (Play.isProd) {
-
+    // To bypass this logic just access the /enable-dev-access URL to create the cookie!
+    // Acccess /disable-dev-access to remove the cookie
+    if (rh.path.endsWith("/enable-dev-access") || rh.path.endsWith("/disable-dev-access")) {
+      next(rh)
+    } else if (rh.cookies.exists(cookie => cookie.name == "DEV_ACCESS")) {
+      next(rh)
+      // We need to let the CSS, JS and IMG assets of the countdown page be rendered!
+    } else if (rh.path.endsWith(".css") || rh.path.endsWith(".js") || rh.path.endsWith(".png") || rh.path.endsWith(".jpg")) {
+      next(rh)
+    } else {
       val now = new DateTime()
       now match {
-        case t if t.isAfter(launchTime) && t.isBefore(launchTime.plusMinutes(1)) =>
-          Future.successful {
-            // TODO Rafa
-            Ok(views.html.index(Seq.empty, Seq.empty, Seq.empty))
-          }
-        case t if t.isAfter(launchTime.plusMinutes(1)) =>
+        case t if t.isAfter(launchTime) =>
           next(rh)
         case t if t.isBefore(launchTime) =>
           Future.successful {
             Ok(views.html.countdown())
           }
       }
-
-    } else {
-      next(rh)
     }
   }
 }
